0bf4ecdfde90c16968a574bb45393af34feead1b
kotenkoI <iryna.herashchenko@gmail.com>
2024-06-28 15:33:41 +0300

    added

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const inputField = document.querySelector('input#name-input');
-const outputField = document.querySelector('span#name-output');
-
-inputField.addEventListener('input', e => {
-    outputField.textContent = e.target.value;
-    if (!e.target.value) {
-        outputField.textContent = 'Anonymous';
-    }
-});
